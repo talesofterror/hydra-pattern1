16c959bca80cc3c364bd9da3c2f51196c062febc
talesofterror <mich.rivera84@gmail.com>
2022-04-25 00:20:16 -0400

    pattern 8 continued

--- pattern4.js
+++ pattern4.js
@@ -1,35 +1,50 @@
+
 var hydra = new Hydra({
     canvas: document.getElementById("myCanvas"),
     detectAudio: false
-  }); 
+  });
 
-  osc(999, spinner(0.2, 0.2), spinner(20, 1))
-    .modulate(osc(3, spinner(0.4, 0.5), 5))
-    .modulate(noise(100, 0))
-    .out(o0);
 
-  src(o0)
-    .add(
-        shape(4, 0.3, 0.01)
-            .scrollX(3, 5.0)
-            .repeatY(3)
-            .repeatX(3)
-            .kaleid(6)
-            .rotate(0, 1.5)
-    )
-    .diff(
-        osc(100, 0, 0) 
-            .rotate(1.6, 0)        
-            .thresh(0.8)
-            .kaleid(6)
-            .rotate(0, 1.5)
-            .color(1, 0.5, 0)
-    )
-        
-    .out(o1);
+s0.initVideo("assets/mario-nutting-clipped2.mp4");
 
-    render(o1);
+src(s0).posterize(50, 4.5).out(o0)
 
-function spinner(v, v2) {
-    return () => Math.sin((v) - v2);
-}
+src(o0)
+  .thresh(0.1)
+  .invert()
+  .out(o2)
+
+osc(12, 1, 2).pixelate(10, 10)
+  .modulate(o0)
+  .diff(o0)
+  .modulate(s0, () => Math.sin(time/0.5) * 0.5)
+  .sub(o2)
+  .mult(o3, 1.5)
+  .add(o2, 1).invert().luma(0.2, 0.01)
+  .mult(o0, 0.5)
+  .out(o1)
+
+  shape(5, 0.005, 0.6)
+    .scrollX(0.2).scrollY(0.2)
+    .kaleid(10)
+    .thresh(0.3)
+    .rotate(1, 1).invert()
+    // .sub(osc(50, 0.2, 5).thresh(0.6).rotate(0.9))
+    // .add(osc(50, 0.1).thresh(0.6).rotate(0.3))
+      .mult(o2).invert()
+    
+      
+
+    .modulate( shape(5, 0.005, 0.5)
+      .scroll(0.2, 0.2)
+      // .thresh(1)
+      .kaleid(2).rotate(1, 1.5).color(1, 1, 1)    )
+    // .diff( shape(5, 0.005, 0.5)
+    // .scroll(0.2, 0.2)
+    // // .thresh(1)
+    // .kaleid(1).rotate(1, 2).color(1, 1, 1)    )
+    // .diff(osc(1).rotate(1, 1) )
+  // .invert()
+  .out(o3)
+
+ render()
---
+++ pattern8.js
@@ -0,0 +1,48 @@
+var hydra = new Hydra({
+    canvas: document.getElementById("myCanvas"),
+    detectAudio: false
+  });  
+
+  var oscSize = 50;
+  var oscSize2 = oscSize * 0.8;
+
+  osc(oscSize2, 0, 0).
+    luma( () => Math.abs(Math.sin(time*0.01)), 1).
+    add(osc(oscSize, ()=>Math.sin(-0.05), 10)).
+    posterize(1, 1).
+    modulate(osc(100, ()=>Math.sin(-0.05), 0.5).luma(0, 0).
+    rotate(1.57)).
+    add(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
+    out(o0)
+
+    osc(oscSize2, 0.05, 0).
+    luma( () => Math.abs(Math.sin(50)), 1).
+    add(osc(oscSize, 0, 10)).
+    posterize(0.8, 1).
+    modulate(osc(100, 0.6, 0.5).luma(0, 0).
+    rotate(1.57, () => Math.sin(-30))).
+    modulate(osc(oscSize2, 0, 0).
+      rotate(0, () => Math.abs(Math.sin(30)))).
+    mult(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
+    out(o1)
+
+    osc(8, 0.5, 0).
+    scrollX(0.5).
+    luma( () => Math.abs(Math.sin(time*0.5)), 1).
+    add(osc(8, 0.5, 10)).
+    posterize(0.8, 1).
+    modulate(osc(0.4, 10, 0.5).luma(1, 0).
+    rotate(1.57, () => Math.sin(-30))).
+    modulate(osc(8, 2, 0).
+      rotate(2, () => 1 + Math.cos(30))).
+    // luma(0, 1).
+    out(o2)
+
+    src(o0)
+    .diff(o1).invert()
+    .diff(o2)
+    .out(o3)
+
+    render();
+
+    setResolution(1080, 1080);
