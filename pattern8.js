f1b1088416a2cc9b3c6f67c019858f6f932de686
mike <mich.rivera84@gmail.com>
2022-04-28 22:47:15 -0400

    pointless tweaking at this point maybe

--- pattern8.js
+++ pattern8.js
@@ -5,27 +5,35 @@ var hydra = new Hydra({
 
   var oscSize = 50;
   var oscSize2 = oscSize * 0.8;
+  var steadyRingSpeed = 5.5
+  var steadyRingAmp = 0.5;
+  var ringSpeedMod = 3.5;
 
   osc(oscSize2, 0, 0).
-    luma( () => Math.abs(Math.sin(time*0.01)), 1).
+    luma( () => Math.abs(Math.sin(time* 0.05)), 1).
     add(osc(oscSize, ()=>Math.sin(-0.05), 10)).
     posterize(1, 1).
-    modulate(osc(100, ()=>Math.sin(-0.05), 0.5).luma(0, 0).
+    modulate(osc(100, ()=>Math.sin(0.05), 0.5).luma(0, 0).
     rotate(1.57)).
     add(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
+    
     out(o0)
 
-  osc(oscSize2, 0.01, 0).
+  osc(oscSize2 * 2, 0.01, 0).
     luma( () => Math.abs(Math.sin(50)), 1).
     add(osc(oscSize, 0, 10)).
     posterize(0.8, 1).
     modulate(osc(100, 0.6, 0.5).luma(0, 0).
-    rotate(1.57, () => Math.sin(-25))).
+    rotate(1.57, () => Math.sin(-30))).
     modulate(osc(oscSize2, 0, 0).
       rotate(0, () => Math.abs(Math.sin(30)))).
     mult(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
-    sub(shape(100, () => Math.abs(0.3 * Math.sin(time * 0.4) + 1), 0.005).scale(1, 0.5, 1)).
-      add(shape(100, () => Math.abs(0.3 * Math.sin(time * 0.4) + 1.3), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1), 0.005).scale(1, 0.5, 1)).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.2), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.4), 0.005).scale(1, 0.5, 1)).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.5), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.6), 0.005).scale(1, 0.5, 1)).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.7), 0.005).scale(1, 0.5, 1)).
     out(o1)
 
   osc(8, 0.5, 0).
@@ -35,26 +43,34 @@ var hydra = new Hydra({
       ).
     posterize(0.8, 1).
     modulate(osc(0.4, 10, 0.5).luma(1, 0).
-      rotate(1.57, () => Math.sin(-30))
+      rotate(1.57, 11.5)
       ).
     modulate(osc(8, 2, 0).
-      rotate(2, () => 1 + Math.cos(30))).
+      rotate(2, 1.5)).
     // luma(0, 1).
     rotate(-1.5, 0).
     kaleid(1).
-    sub(shape(100, () => Math.abs(0.5 * Math.sin(time * 0.5) + 1.4), 0.005).scale(1, 0.5, 1)).
-    add(shape(75, () => Math.abs(0.5 * Math.sin(time * 0.4) + 1.2), 0.005).scale(1, 0.5, 1)).
-    sub(shape(50, () => Math.abs(0.3 * Math.sin(time * 0.3) + 1.6), 0.005).scale(1, 0.5, 1)).
-    add(shape(25, () => Math.abs(0.3 * Math.sin(time * 0.2) + 1.8), 0.005).scale(1, 0.5, 1)).
+    mult(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1), 0.005).scale(1, 0.5, 1)).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.2), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.4), 0.005).scale(1, 0.5, 1)).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.5), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.6), 0.005).scale(1, 0.5, 1)).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.7), 0.005).scale(1, 0.5, 1)).
+    // sub(shape(100, () => Math.abs(0.5 * Math.sin(time * 3.5) + 1.4), 0.005).scale(1, 0.5, 1)).
+    // add(shape(100, () => Math.abs(0.5 * Math.sin(time * 3.4) + 1.2), 0.005).scale(1, 0.5, 1)).
+    // sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * 3.3) + 1.6), 0.005).scale(1, 0.5, 1)).
+    // add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * 3.2) + 1.8), 0.005).scale(1, 0.5, 1)).
     out(o2)
 
-  src(o0)
-    .diff(o1)
-      .invert()
-    .diff(o2)
-    .out(o3)
+  src(o0).
+    diff(o1).
+      invert().
+    diff(o2).
+    // modulatePixelate(o0).
+    out(o3)
 
-    render();
+    render(o3);
 
     setResolution(1080, 1080);
 
