1bd69edf53e3725548b6f566103822c364680d3c
mike <mich.rivera84@gmail.com>
2022-04-30 00:00:49 -0400

    I think I'm done

--- pattern8.js
+++ pattern8.js
@@ -3,20 +3,27 @@ var hydra = new Hydra({
     detectAudio: false
   });  
 
-  var oscSize = 50;
-  var oscSize2 = oscSize * 0.8;
-  var steadyRingSpeed = 5.5
-  var steadyRingAmp = 0.5;
-  var ringSpeedMod = 3.5;
+  var oscSize = 50
+  var oscSize2 = oscSize * 0.8
+  var steadyRingSpeed = 2.5
+  var steadyRingAmp = 0.5
+  var ringSpeedMod = 1.5
+
+  var ring = (amp, speed, mod, phase) => 
+    Math.abs(amp * Math.sin(time * speed + mod)) + phase
+  
+  var sine = (speed) => Math.sin(speed)
 
   osc(oscSize2, 0, 0).
     luma( () => Math.abs(Math.sin(time* 0.05)), 1).
     add(osc(oscSize, ()=>Math.sin(-0.05), 10)).
     posterize(1, 1).
     modulate(osc(100, ()=>Math.sin(0.05), 0.5).luma(0, 0).
-    rotate(1.57)).
-    add(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
-    
+    rotate(1.57)
+      ).
+    add(shape(100, 0.5, 0.005).scale(1, 0.5, 1)
+      ).
+    // luma().
     out(o0)
 
   osc(oscSize2 * 2, 0.01, 0).
@@ -28,11 +35,11 @@ var hydra = new Hydra({
     modulate(osc(oscSize2, 0, 0).
       rotate(0, () => Math.abs(Math.sin(30)))).
     mult(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
-    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1), 0.005).scale(1, 0.5, 1)).
-      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.2), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, 0.7, 0.005).scale(1, 0.5, 1).modulate(noise(10, 0.5, 0).rotate(1.59))).
+      add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 0.9), 0.005).scale(1, 0.5, 1).modulate(noise(10, 0.5, 0).rotate(1.59))).
     sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.4), 0.005).scale(1, 0.5, 1)).
       add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.5), 0.005).scale(1, 0.5, 1)).
-    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.6), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.6), 0.005).scale(1, 0.5, 1).modulate(noise(10, 0.5, 0).rotate(1.59))).
       add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed) + 1.7), 0.005).scale(1, 0.5, 1)).
     out(o1)
 
@@ -47,26 +54,22 @@ var hydra = new Hydra({
       ).
     modulate(osc(8, 2, 0).
       rotate(2, 1.5)).
-    // luma(0, 1).
     rotate(-1.5, 0).
     kaleid(1).
     mult(shape(100, 0.5, 0.005).scale(1, 0.5, 1)).
-    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, 1.5, 0.005).scale(1, 0.5, 1).modulate(noise(10, 0.5, 0).rotate(1.59))).
       add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.2), 0.005).scale(1, 0.5, 1)).
-    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.4), 0.005).scale(1, 0.5, 1)).
+    sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.4), 0.005).scale(1, 0.5, 1).modulate(noise(10, 0.5, 0).rotate(1.59))).
       add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.5), 0.005).scale(1, 0.5, 1)).
     sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.6), 0.005).scale(1, 0.5, 1)).
       add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * steadyRingSpeed + ringSpeedMod) + 1.7), 0.005).scale(1, 0.5, 1)).
-    // sub(shape(100, () => Math.abs(0.5 * Math.sin(time * 3.5) + 1.4), 0.005).scale(1, 0.5, 1)).
-    // add(shape(100, () => Math.abs(0.5 * Math.sin(time * 3.4) + 1.2), 0.005).scale(1, 0.5, 1)).
-    // sub(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * 3.3) + 1.6), 0.005).scale(1, 0.5, 1)).
-    // add(shape(100, () => Math.abs(steadyRingAmp * Math.sin(time * 3.2) + 1.8), 0.005).scale(1, 0.5, 1)).
     out(o2)
 
   src(o0).
     diff(o1).
       invert().
     diff(o2).
+    
     // modulatePixelate(o0).
     out(o3)
 
